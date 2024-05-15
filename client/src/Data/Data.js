16dbb4e157c3e7a2bec0a0aab8f785c3f94790ea
unknown <arg2002suku@gmail.com>
2024-05-15 17:43:05 +0530

    commit

---
+++ client/src/Data/Data.js
@@ -0,0 +1,27 @@
+export const SidebarMenu = [
+  {
+    name: "Home",
+    path: "/",
+    icon: "fa fa-thin fa-house",
+  },
+  {
+    name: "Appointments",
+    path: "/appointments",
+    icon: "fa fa-thin fa-list",
+  },
+  {
+    name: "Apply Doctor",
+    path: "/apply-doctor",
+    icon: "fa fa-thin fa-user-doctor",
+  },
+  {
+    name: "Profile",
+    path: "/profile",
+    icon: "fa fa-thin fa-user",
+  },
+  {
+    name: "Logout",
+    path: "/logout",
+    icon: "fa fa-thin fa-right-from-bracket",
+  },
+];
